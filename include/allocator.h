4d6bd6f058b2eb54865f0fd0ef6a1c4263dcfbaa
timothy <caveiroussight@gmail.com>
2025-05-16 18:18:43 +0600

    fixed allocator struct

--- include/allocator.h
+++ include/allocator.h
@@ -1,28 +1,29 @@
 #ifndef ALLOCATOR_H
 #define ALLOCATOR_H
-#include <stddef.h>
+
+#include <stddef.h> 
+
+typedef struct Allocator {
+    void* (*alloc)(struct Allocator* allocator, size_t size);
+    void (*free)(struct Allocator* allocator, void* ptr);
+} Allocator;
 
 typedef struct {
+    Allocator interface;  
     void* memory_block;
     size_t total_size;
     size_t offset;
 } LinearAllocator;
 
 typedef struct {
+    Allocator interface;  
     void* memory_block;
     size_t block_size;
     size_t num_blocks;
     void* free_list;
 } PoolAllocator;
 
-void pool_init(PoolAllocator* allocator, size_t block_size, size_t num_blocks);
-void* pool_alloc(PoolAllocator* allocator);
-void pool_free(PoolAllocator* allocator, void* ptr);
-void pool_free_all(PoolAllocator* allocator);
-
 void linear_init(LinearAllocator* allocator, size_t size);
-void* linear_alloc(LinearAllocator* allocator, size_t size);
-void linear_reset(LinearAllocator* allocator);
-void linear_free(LinearAllocator* allocator);
+void pool_init(PoolAllocator* allocator, size_t block_size, size_t num_blocks);
 
-#endif // ALLOCATOR_H
+#endif 
--- src/allocator.c
+++ src/allocator.c
@@ -1,42 +1,60 @@
 #include "allocator.h"
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+
+static void* linear_alloc_impl(Allocator* allocator, size_t size) {
+    LinearAllocator* lin_alloc = (LinearAllocator*)allocator;
+    if (lin_alloc->offset + size > lin_alloc->total_size) {
+        return NULL;
+    }
+    
+    void* ptr = (char*)lin_alloc->memory_block + lin_alloc->offset;
+    lin_alloc->offset += size;
+    return ptr;
+}
+
+static void linear_free_impl(Allocator* allocator, void* ptr) {
+    (void)allocator;
+    (void)ptr;
+}
 
 void linear_init(LinearAllocator* allocator, size_t size) {
+    allocator->interface.alloc = linear_alloc_impl;
+    allocator->interface.free = linear_free_impl;
     allocator->memory_block = malloc(size);
     allocator->total_size = size;
     allocator->offset = 0;
 }
 
-void* linear_alloc(LinearAllocator* allocator, size_t size) {
-    if (allocator->offset + size > allocator->total_size) {
+static void* pool_alloc_impl(Allocator* allocator, size_t size) {
+    PoolAllocator* pool_alloc = (PoolAllocator*)allocator;
+    (void)size;
+    
+    if (pool_alloc->free_list == NULL) {
         return NULL;
     }
     
-    void* ptr = (char*)allocator->memory_block + allocator->offset;
-    allocator->offset += size;
+    void* ptr = pool_alloc->free_list;
+    pool_alloc->free_list = *(void**)ptr;
     return ptr;
 }
 
-void linear_reset(LinearAllocator* allocator) {
-    allocator->offset = 0;
+static void pool_free_impl(Allocator* allocator, void* ptr) {
+    if (ptr == NULL) return;
+    
+    PoolAllocator* pool_alloc = (PoolAllocator*)allocator;
+    *(void**)ptr = pool_alloc->free_list;
+    pool_alloc->free_list = ptr;
 }
 
-void linear_free(LinearAllocator* allocator) {
-    free(allocator->memory_block);
-    allocator->memory_block = NULL;
-    allocator->total_size = 0;
-    allocator->offset = 0;
-}
-
-// allocator.c
 void pool_init(PoolAllocator* allocator, size_t block_size, size_t num_blocks) {
+    allocator->interface.alloc = pool_alloc_impl;
+    allocator->interface.free = pool_free_impl;
     allocator->block_size = block_size;
     allocator->num_blocks = num_blocks;
     allocator->memory_block = malloc(block_size * num_blocks);
     allocator->free_list = allocator->memory_block;
     
-    // Initialize free list
     void* current = allocator->memory_block;
     for (size_t i = 0; i < num_blocks - 1; ++i) {
         void** next = (void**)((char*)current + block_size);
@@ -44,27 +62,4 @@ void pool_init(PoolAllocator* allocator, size_t block_size, size_t num_blocks) {
         current = (char*)current + block_size;
     }
     *(void**)current = NULL;
-}
-
-void* pool_alloc(PoolAllocator* allocator) {
-    if (allocator->free_list == NULL) {
-        return NULL;
-    }
-    
-    void* ptr = allocator->free_list;
-    allocator->free_list = *(void**)ptr;
-    return ptr;
-}
-
-void pool_free(PoolAllocator* allocator, void* ptr) {
-    if (ptr == NULL) return;
-    
-    *(void**)ptr = allocator->free_list;
-    allocator->free_list = ptr;
-}
-
-void pool_free_all(PoolAllocator* allocator) {
-    free(allocator->memory_block);
-    allocator->memory_block = NULL;
-    allocator->free_list = NULL;
 }
